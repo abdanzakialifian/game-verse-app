608782ccd5a1407a92d5100d8ad8d8db60976778
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2026-06-06 14:22:35 +0700

    feature : add image, title and platform information in home screen

--- shared/src/commonMain/kotlin/com/gameverse/app/presentation/home/HomeScreen.kt
+++ shared/src/commonMain/kotlin/com/gameverse/app/presentation/home/HomeScreen.kt
@@ -1,20 +1,46 @@
 package com.gameverse.app.presentation.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
+import com.gameverse.app.common.Platform
 import com.gameverse.app.component.GVSearch
 import com.gameverse.app.data.response.GamesResponse
+import com.gameverse.app.theme.GameVerseColor
+import com.gameverse.app.theme.GameVerseShapes
 import com.gameverse.app.theme.GameVerseTheme
+import com.gameverse.app.theme.GameVerseTypography
+import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
+import kotlin.collections.map
+import kotlin.collections.orEmpty
 
 @Composable
 fun HomeScreen(
@@ -50,17 +76,84 @@ private fun HomeContent(
             }
         )
 
-        GameItem(uiState.gamesData)
+        Games(items = uiState.gamesData?.results)
     }
 }
 
 @Composable
-private fun GameItem(data: GamesResponse?) {
-    data?.results?.forEach {
-        AsyncImage(
-            model = it.backgroundImage,
-            contentDescription = null
-        )
+private fun Games(
+    modifier: Modifier = Modifier,
+    items: List<GamesResponse.ResultsItem>?
+) {
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(vertical = 16.dp)
+    ) {
+        items(items.orEmpty(), key = { it.id ?: 0 }) { result ->
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                shape = GameVerseShapes.large,
+                colors = CardDefaults.cardColors(contentColor = GameVerseColor.secondary)
+            ) {
+                Column {
+                    AsyncImage(
+                        modifier = Modifier.fillMaxWidth().height(130.dp),
+                        model = result.backgroundImage,
+                        contentScale = ContentScale.FillWidth,
+                        contentDescription = null,
+                        filterQuality = FilterQuality.Medium,
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Platforms(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        platforms = result.platforms.orEmpty()
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        text = result.name.orEmpty(),
+                        style = GameVerseTypography.titleSmall.copy(fontWeight = FontWeight.Bold),
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        text = "View More",
+                        style = GameVerseTypography.labelMedium.copy(textDecoration = TextDecoration.Underline),
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Platforms(
+    modifier: Modifier = Modifier,
+    platforms: List<GamesResponse.ResultsItem.PlatformsItem>
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(6.dp)
+    ) {
+        val platformIds = platforms.map { it.platform?.id ?: 0 }
+        val platformIcons = Platform.iconFromIds(platformIds)
+        platformIcons.forEach { iconDrawable ->
+            Icon(
+                modifier = Modifier.size(16.dp),
+                painter = painterResource(iconDrawable),
+                contentDescription = null,
+                tint = Color.White
+            )
+        }
     }
 }
 
@@ -69,7 +162,59 @@ private fun GameItem(data: GamesResponse?) {
 private fun HomeContentPreview() {
     GameVerseTheme {
         HomeContent(
-            uiState = HomeReducer.State(),
+            uiState = HomeReducer.State(
+                gamesData = GamesResponse(
+                    results = List(5) {
+                        GamesResponse.ResultsItem(
+                            id = it,
+                            backgroundImage = "https://media.rawg.io/media/games/20a/20aa03a10cda45239fe22d035c0ebe64.jpg",
+                            name = "Grand Theft Auto V",
+                            platforms = listOf(
+                                GamesResponse.ResultsItem.PlatformsItem(
+                                    platform = GamesResponse.ResultsItem.Platform(
+                                        id = 21
+                                    )
+                                ),
+                                GamesResponse.ResultsItem.PlatformsItem(
+                                    platform = GamesResponse.ResultsItem.Platform(
+                                        id = 80
+                                    )
+                                ),
+                                GamesResponse.ResultsItem.PlatformsItem(
+                                    platform = GamesResponse.ResultsItem.Platform(
+                                        id = 19
+                                    )
+                                ),
+                                GamesResponse.ResultsItem.PlatformsItem(
+                                    platform = GamesResponse.ResultsItem.Platform(
+                                        id = 3
+                                    )
+                                ),
+                                GamesResponse.ResultsItem.PlatformsItem(
+                                    platform = GamesResponse.ResultsItem.Platform(
+                                        id = 4
+                                    )
+                                ),
+                                GamesResponse.ResultsItem.PlatformsItem(
+                                    platform = GamesResponse.ResultsItem.Platform(
+                                        id = 6
+                                    )
+                                ),
+                                GamesResponse.ResultsItem.PlatformsItem(
+                                    platform = GamesResponse.ResultsItem.Platform(
+                                        id = 18
+                                    )
+                                ),
+                                GamesResponse.ResultsItem.PlatformsItem(
+                                    platform = GamesResponse.ResultsItem.Platform(
+                                        id = 100
+                                    )
+                                )
+                            )
+                        )
+                    }
+                )
+            ),
             onIntent = {}
         )
     }
