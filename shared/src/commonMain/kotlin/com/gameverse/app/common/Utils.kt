b82da59aaff4540b861d9d7914b02f4bb444c263
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2026-06-11 23:23:49 +0700

    chore : added slide animation function for navigation animation

--- shared/src/commonMain/kotlin/com/gameverse/app/common/Utils.kt
+++ shared/src/commonMain/kotlin/com/gameverse/app/common/Utils.kt
@@ -1,5 +1,10 @@
 package com.gameverse.app.common
 
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.navigation3.runtime.metadata
+import androidx.navigation3.ui.NavDisplay
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import kotlin.time.Clock
@@ -15,6 +20,23 @@ object Utils {
             in 12..16 -> Greeting.AFTERNOON
             in 17..20 -> Greeting.EVENING
             else -> Greeting.NIGHT
+        }
+    }
+
+    fun slideAnimation(): Map<String, Any> = metadata {
+        put(NavDisplay.TransitionKey) {
+            slideInHorizontally(initialOffsetX = { it }) togetherWith
+                    slideOutHorizontally(targetOffsetX = { -it })
+        }
+
+        put(NavDisplay.PopTransitionKey) {
+            slideInHorizontally(initialOffsetX = { -it }) togetherWith
+                    slideOutHorizontally(targetOffsetX = { it })
+        }
+
+        put(NavDisplay.PredictivePopTransitionKey) {
+            slideInHorizontally(initialOffsetX = { -it }) togetherWith
+                    slideOutHorizontally(targetOffsetX = { it })
         }
     }
 }
