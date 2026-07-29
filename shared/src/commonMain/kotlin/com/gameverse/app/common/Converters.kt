041bc1ee47c3fa78b01707ed263f4058b89308f0
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2026-07-29 21:57:17 +0700

    refactor(shared): add type converters for integer list serialization

--- shared/src/commonMain/kotlin/com/gameverse/app/common/Converters.kt
+++ shared/src/commonMain/kotlin/com/gameverse/app/common/Converters.kt
@@ -1,6 +1,8 @@
 package com.gameverse.app.common
 
 import androidx.room.TypeConverter
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.json.Json
 
 class Converters {
@@ -12,5 +14,15 @@ class Converters {
     @TypeConverter
     fun toStringList(value: String): List<String> {
         return Json.decodeFromString(value)
+    }
+
+    @TypeConverter
+    fun fromIntList(value: List<Int>): String {
+        return Json.encodeToString(ListSerializer(Int.serializer()), value)
+    }
+
+    @TypeConverter
+    fun toIntList(value: String): List<Int> {
+        return Json.decodeFromString(ListSerializer(Int.serializer()), value)
     }
 }
