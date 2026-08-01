70dbbfc8cbe0fa9bee1719c129d7ceb02809422c
Abdan Zaki Alifian <abdanzakialifian99@gmail.com>
2026-08-01 23:54:27 +0700

    refactor: add column info annotations to FavoriteEntity properties

--- shared/src/commonMain/kotlin/com/gameverse/app/data/entity/FavoriteEntity.kt
+++ shared/src/commonMain/kotlin/com/gameverse/app/data/entity/FavoriteEntity.kt
@@ -1,14 +1,21 @@
 package com.gameverse.app.data.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity("favorites")
 data class FavoriteEntity(
-    @PrimaryKey val id: Int,
+    @PrimaryKey
+    val id: Int,
+    @ColumnInfo("name") 
     val name: String,
+    @ColumnInfo("background_image")
     val backgroundImage: String,
+    @ColumnInfo("released")
     val released: String,
+    @ColumnInfo("genres")
     val genres: List<String>,
+    @ColumnInfo("platforms")
     val platforms: List<Int>,
 )
